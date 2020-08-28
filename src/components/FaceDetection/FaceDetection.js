e9b12a4347f89617f9f354914bd30f6ea2f8cb4e
M <M@DESKTOP-J0D963F>
2020-08-28 15:38:58 +0300

    face detection front-end

---
+++ src/components/FaceDetection/FaceDetection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FaceDetection.css';
+
+const FaceDetection = ({imageUrl, box}) => {
+  return (
+    <div className="center ma">
+      <div className='absolute ma2'>
+        <img id='inputimage' alt='smt' src={imageUrl} width='500px' height='auto' />
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
+    </div>
+  );
+}
+
+export default FaceDetection;
